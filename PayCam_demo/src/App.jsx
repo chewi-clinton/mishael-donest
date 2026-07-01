c8e686e89f59ffd474181fcdbf0ec9d0d5833b74
Esepongha <esepongha@gmail.com>
2026-07-01 11:32:55 +0100

    commit the  registration page

---
+++ PayCam_demo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PayCamSignup from './components/PayCamSignup (2).jsx'
+
+function App() {
+    return (
+        <>
+            <PayCamSignup />
+        </>
+    )
+}
+
+export default App
